3b815465526fcfa23ae481d5e55019882e0a755d
Eric Musyoka <musyokaeric@live.com>
2023-01-30 18:39:26 +0300

    Middle Top Section

---
+++ WPFUIRecruitmentDashboard/UserControls/InfoCard.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MahApps.Metro.IconPacks;
+
+namespace WPFUIRecruitmentDashboard.UserControls
+{
+    /// <summary>
+    /// Interaction logic for InfoCard.xaml
+    /// </summary>
+    public partial class InfoCard : UserControl
+    {
+        public InfoCard()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(InfoCard));
+
+
+
+        public string Number
+        {
+            get { return (string)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Number.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NumberProperty =
+            DependencyProperty.Register("Number", typeof(string), typeof(InfoCard));
+
+
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsActive.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(InfoCard));
+
+
+
+        public bool IsTechnical
+        {
+            get { return (bool)GetValue(IsTechnicalProperty); }
+            set { SetValue(IsTechnicalProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsTechnical.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsTechnicalProperty =
+            DependencyProperty.Register("IsTechnical", typeof(bool), typeof(InfoCard));
+
+
+    }
+}
