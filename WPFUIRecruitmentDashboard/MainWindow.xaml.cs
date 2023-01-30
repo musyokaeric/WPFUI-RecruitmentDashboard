8a442bede4d2fcfbda5c8e3e8b952872eb588fb0
Eric Musyoka <musyokaeric@live.com>
2023-01-30 16:20:43 +0300

    Side Panel Design
    Designed the side panel. Installed MahApps package used Dependency Injection to design the buttons and triggers

--- WPFUIRecruitmentDashboard/MainWindow.xaml.cs
+++ WPFUIRecruitmentDashboard/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace WPFUIRecruitmentDashboard
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
     }
 }
---
+++ WPFUIRecruitmentDashboard/UserControls/Menubutton.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MahApps.Metro.IconPacks;
+
+namespace WPFUIRecruitmentDashboard.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Menubutton.xaml
+    /// </summary>
+    public partial class Menubutton : UserControl
+    {
+        public Menubutton()
+        {
+            InitializeComponent();
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get { return (PackIconMaterialKind)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PackIconMaterialKind), typeof(Menubutton));
+
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsActive.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(Menubutton));
+    }
+}
